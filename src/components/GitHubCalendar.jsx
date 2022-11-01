e695ea6b1418d97d070bcdcab3281b92767f328d
masum-raja <razamasum@gmail.com>
2022-11-01 14:54:20 +0530

    Added stats

--- src/components/GitHubCalendar.jsx
+++ src/components/GitHubCalendar.jsx
@@ -20,9 +20,10 @@ const GitHub=()=>{
       >
         <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w text-white">
           <div>
-            <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
-                GitHub Calendar & Stats
+            <p className="text-4xl  font-bold border-b-4 border-gray-500 p-2 inline">
+                Calendar & Stats
             </p>
+            <p className="py-6">Check out my Github Calendar & Streak Stats  </p>
           </div>
         </div>
 
@@ -31,13 +32,13 @@ const GitHub=()=>{
         </div>
 
           <div data-aos="fade-up" className='flex justify-center pt-8 '>
-            <a href="https://github.com/masum-raja/github-readme-streak-stats" className=' px-12 sm:px-0'>
+            <a href="https://github.com/masum-raja" className=' px-12 sm:px-0'>
               <img className='shadow-md hover:scale-105 duration-500  rounded-lg shadow-sky-400'  src="https://github-readme-streak-stats.herokuapp.com?user=masum-raja&theme=dark&hide_border=true&ring=0ABEB8&background=161616&currStreakLabel=DDDDDD&fire=FF0000" alt="" />
             </a>
           </div>
 
           <div data-aos="fade-up" className='flex justify-center pt-12'>
-            <a href="https://github.com/masum-raja/github-readme-stats" className='px-12 sm:px-0'>
+            <a href="https://github.com/masum-raja" className='px-12 sm:px-0'>
               <img className='shadow-md hover:scale-105 duration-500  rounded-lg shadow-sky-400' src="https://github-readme-stats.vercel.app/api?username=masum-raja&show_icons=true&locale=en&theme=react&hide_border=true&bg_color=161616" alt="" />
             </a>
           </div>
